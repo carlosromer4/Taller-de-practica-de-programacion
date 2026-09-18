a09cf42841a3bcc0adf47fb0ed67070b386aa254
carlosromer4 <carlos2252872@correo.uis.edu.co>
2026-09-18 11:30:41 -0500

    Add files via upload

---
+++ Taller-de-practica-de-programacion/src/Puntos.java
@@ -0,0 +1,25 @@
+public class Puntos {
+    void holaMundo() {
+        System.out.printf("hola mundo");
+    }
+
+    void holamundo() {
+        System.out.printf("hola mundo");
+    }
+}
+
+
+
+    public static double calcularPromedio(double n1, double n2, double n3, double n4, double n5){
+        return n1 * 0.15
+                + n2 * 0.20
+                + n3 * 0.15
+                + n4 * 0.30
+                + n5 * 0.20;
+
+
+    }
+    void holaMundo(){
+        System.out.printf("hola mundo");
+    }
+}
